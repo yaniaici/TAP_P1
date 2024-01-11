bcd227a8593d8973381de97b87ce4a85bb848298
Adria Chipre <adria.chipre@estudiants.urv.cat>
2024-01-11 16:55:05 +0100

    he hecho un poco más

--- src/scala/Controller.scala
+++ src/scala/Controller.scala
@@ -1,8 +1,23 @@
 package scala
 
+import scala.Invoker
+
+import scala.reflect.internal.util.Collections
+
 class Controller {
-  def invoke(action: String, params: Object):Object  = {
-    val result = Array()
-    return result
+  val invokers: Seq[Invoker] => List[Invoker] = List[Invoker]
+
+  def invoke(actionName: String, params: Object):List[Object] = {
+    val actionNames = convertActionNameToList(actionName)
+    val selectedInvoker = findAvaiableInvoker(128)
+    throw new Exception
   }
+  def findAvaiableInvoker(requiredMemoryMB: Int) = {
+    println(invokers)
+    for (invoker <- invokers) {
+
+
+    }
+  }
+  def convertActionNameToList(actionName: String): List[String] = List(actionName)
 }
--- src/scala/Invoker.scala
+++ src/scala/Invoker.scala
@@ -3,17 +3,31 @@ package scala
 import scala.collection.mutable
 
 class Invoker (totalMemory:Int, controller:Controller, invokerID:String){
+  var usedMemory = 0
   val totalMemoryMB:Int = totalMemory
   val controler:Controller = controller
   val invokerId:String = invokerID
-  val actions = new mutable.HashMap[String,Function[Object,Object]]()
+  val actions = new mutable.HashMap[String,Function[Object,Object]]
+
+  def getInvokerId() = invokerID
+
+  def getController() = controller
+  def registerAction(actionName:String, action:Function[Object,Object], memoryMB: Int ) = {
+    if(getFreeMemory() > memoryMB){
+      actions.put(actionName,action)
+      usedMemory = usedMemory + memoryMB
+      println(usedMemory)
+    }else{
+      println("No hay suficiente memoria para registrar la acción '\" + actionName + \"'.\"")
+    }
+  }
   def invokeAction(actionName:String, params:Object):Object = {
     if(!hasAction(actionName)){
       throw new Exception("Action not found")
     }
-    val action = actions.get(actionName)
+    val action = actions(actionName)
     try {
-      action
+      action.apply(params)
     } catch {
       case e: Exception =>
         throw new RuntimeException("Error al ejecutar la acción", e)
@@ -24,4 +38,10 @@ class Invoker (totalMemory:Int, controller:Controller, invokerID:String){
     actions.contains(action)
   }
 
+  def getFreeMemory() = totalMemoryMB - usedMemory
+
+  def getUsedMemory() = usedMemory
+
+
+  override def toString = s"Invoker(InvokerId = $invokerId,Total Memory = $totalMemoryMB, Used Memory = $usedMemory, Actions = $actions)"
 }
